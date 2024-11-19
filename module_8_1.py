517fd6e3f8381eaa6cc5dbb826e92a4b89f262ec
Ludmila <foxnlo77@gmail.com>
2024-11-19 17:59:16 +0700

    Исправлен файл module_8_1.py

--- module_8_1.py
+++ module_8_1.py
@@ -4,14 +4,14 @@ def add_everything_up(a, b):
     except TypeError:
         return str(a) + str(b)
     else:
-        if isinstance(c, int) or isinstance(c, float):
+        if isinstance(c, float):
             c = round(c, 3)
         return c
 
 
 print(add_everything_up(123.456, 'строка'))
 print(add_everything_up('яблоко', 4215))
-print(add_everything_up(123.456, 7))
+print((add_everything_up(123.456, 7)))
 
 
 
